40efb20d483cada3794f5c4397f577b6b9d2efa3
apxfinance <99514264+apxfinance@users.noreply.github.com>
2022-03-21 14:21:46 +0100

    Update TombFinance.ts

--- src/tomb-finance/TombFinance.ts
+++ src/tomb-finance/TombFinance.ts
@@ -292,9 +292,9 @@ export class TombFinance {
     }
     const rewardPerSecond = await poolContract.AsharePerSecond();
     if (depositTokenName === 'APEX-FTM-LP') {
-      return rewardPerSecond.mul(60).div(1000);
+      return rewardPerSecond.mul(600).div(1000);
     } else if (depositTokenName === 'ASHARE-FTM-LP'){
-      return rewardPerSecond.mul(5).div(1000);
+      return rewardPerSecond.mul(50).div(1000);
     } else if (depositTokenName === 'APEX'){
       return rewardPerSecond.mul(350).div(1000);
     }
